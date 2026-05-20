893f444a04fbaecb8d572e165ebb4c2cb6936f2d
Vivek Rajput <work.tvivekrajput@gmail.com>
2026-05-20 14:37:12 +0530

    change logged

--- backend/server.js
+++ backend/server.js
@@ -4,5 +4,5 @@ const connectDB = require("./src/db/db")
 connectDB()
 
 app.listen(process.env.PORT || 3000 , () => {
-    console.log("server is running on port 3000")
+    console.log(`server is running on port ${process.env.PORT}`)
 })
