0f913654d1f7a5c5ea7c6ef7b3b84ab82cd6ac27
Vivek Rajput <work.tvivekrajput@gmail.com>
2026-05-20 15:25:37 +0530

    a extra slash typo is fixed from cors policy

--- backend/src/app.js
+++ backend/src/app.js
@@ -10,7 +10,7 @@ const cors = require("cors")
 const app = express()
 
 app.use(cors({
-    origin: ["http://localhost:5173", "https://food-reel-plum.vercel.app/"],
+    origin: ["http://localhost:5173", "https://food-reel-plum.vercel.app"],
     credentials: true
 }))
 app.use(express.json())
